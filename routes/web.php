64d6aa8b677b8b1fdc420a9e360bea489500788e
MacBook <macbook@MacBooks-MacBook-Pro.local>
2026-07-30 14:29:57 +0700

    fix: remove signed middleware from registration success route to allow xendit redirect

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::get('/daftar-academy', [RegistrationController::class, 'createAcademy'])-
 Route::get('/daftar-ss', [RegistrationController::class, 'createSs'])->name('register-ss.create');
 Route::post('/pendaftaran', [RegistrationController::class, 'store'])->middleware('throttle:10,1')->name('pendaftaran.store');
 Route::post('/pendaftaran/check-email', [RegistrationController::class, 'checkEmail'])->middleware('throttle:30,1')->name('pendaftaran.check-email');
-Route::get('/pendaftaran/sukses/{siswa}', [RegistrationSuccessController::class, 'show'])->name('registration.success')->middleware('signed');
+Route::get('/pendaftaran/sukses/{siswa}', [RegistrationSuccessController::class, 'show'])->name('registration.success');
 Route::post('/promo/validate', [RegistrationController::class, 'validatePromoCode'])->middleware('throttle:30,1')->name('promo.validate');
 
 Route::get('/dashboard', function () {
