6646dd7b467ff27d32a3158553a2f2d1fb7106ed
MacBook <macbook@MacBooks-MacBook-Pro.local>
2026-09-17 14:07:34 +0700

    Keep checkmark loaded and static

--- resources/js/Pages/Admin/Analytics/Index.vue
+++ resources/js/Pages/Admin/Analytics/Index.vue
@@ -33,7 +33,7 @@
                                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                             </svg>
                             <!-- Success Checkmark -->
-                            <svg v-else-if="isSuccess" class="text-green-500 h-6 w-6 animate-pulse" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg v-else-if="isSuccess" class="text-green-500 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                             </svg>
                         </div>
@@ -163,13 +163,11 @@ const formatCurrency = (value) => {
 };
 
 const isLoading = ref(false);
-const isSuccess = ref(false);
-let successTimeout = null;
+const isSuccess = ref(true); // By default show checkmark when page is loaded
 
 const updateFilters = () => {
     isLoading.value = true;
     isSuccess.value = false;
-    if (successTimeout) clearTimeout(successTimeout);
 
     form.get(route('admin.analytics.index'), {
         preserveState: true,
@@ -178,11 +176,6 @@ const updateFilters = () => {
         onFinish: () => {
             isLoading.value = false;
             isSuccess.value = true;
-            
-            // Remove success checkmark after 3 seconds
-            successTimeout = setTimeout(() => {
-                isSuccess.value = false;
-            }, 3000);
         }
     });
 };
